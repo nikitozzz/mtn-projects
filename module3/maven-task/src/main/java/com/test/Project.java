42f3009d6939a5f30bfb5d498d83e7edcfd8e815
nikitozzz <ryazan3@gmail.com>
2017-07-26 07:57:30 -0400

    sonar project.properties

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -13,8 +13,8 @@ class Project {
     System.out.println("Hellow MTN");
   }
   
-  public static void main(String[] args) {
-    System.out.println("Hello World");
+  public static void test() {
+    System.out.println("Hellow Jenkins");
   }
 
 }
